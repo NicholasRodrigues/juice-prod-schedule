682cb897edfe85082b36d62b290b343e1631c2a9
felipeduarte <felipeduartea04@gmail.com>
2024-10-13 18:12:15 -0300

    Quebrando o Tabu

--- include/neighborhoods.h
+++ include/neighborhoods.h
@@ -18,5 +18,13 @@ bool reinsertionNeighborhood(ScheduleData &scheduleData, const std::vector<Order
 bool twoOptNeighborhood(ScheduleData &scheduleData, const std::vector<Order> &orders,
                         const std::vector<std::vector<int>> &setupTimes,
                         const std::vector<int> &initialSetupTimes);
+struct TabuMove {
+    int i;  // Índice inicial do primeiro bloco
+    int j;  // Índice inicial do segundo bloco
+    int l;  // Tamanho do bloco
 
+    bool operator==(const TabuMove& other) const {
+        return i == other.i && j == other.j && l == other.l;
+    }
+};
 #endif // NEIGHBORHOODS_H
